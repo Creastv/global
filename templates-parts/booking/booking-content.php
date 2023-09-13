dc41f1934f55cb893b71dc0a623b16dfc4b5401e
Creastv <piotr@roial.pl>
2023-09-13 13:51:48 +0200

    slider-gallery

--- templates-parts/booking/booking-content.php
+++ templates-parts/booking/booking-content.php
@@ -5,7 +5,7 @@ $idCar  = intval($_COOKIE['carID']);
     <div class="booking-page__content <?php echo $idCar ? " " : "disabled"; ?> ">
         <?php get_template_part( 'templates-parts/booking/booking', 'form' ); ?>
     </div>
-    <div class="booking-page__resume">
+    <div class="booking-page__resume <?php echo $idCar ? " " : "disabled"; ?> ">
         <?php get_template_part( 'templates-parts/booking/booking', 'resumen' ); ?>
     </div>
 </div>
